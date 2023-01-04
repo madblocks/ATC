670f05f463722929dd4848319d1194e98db7a6fa
Andrew Kennedy <akennedy811@icloud.com>
2023-01-03 20:04:30 -0500

    switched next to ts, added and tested Tailwinds

---
+++ next-atc/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
