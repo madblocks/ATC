7065bf9f3ca51292cfe4eba5e233fa81e2f36d75
Andrew Kennedy <akennedy811@icloud.com>
2023-01-04 23:11:28 -0500

    changed name of manufacturers and toos pages

--- next-atc/components/navbar.js
+++ next-atc/components/navbar.js
@@ -10,13 +10,17 @@ export default function Navbar ({home}) {
 
   return (
     <div className="flex bg-gray-700 h-16 items-center px-10">
-      <div className="w-2/4 flex items-center">
+      <div className="w-1/3 flex items-center">
         <Link href="/" className="flex items-center">
           <Logo className="text-5xl fill-green-500 inline-block"/>
           <div className="text-white pl-5 text-4xl font-poppins inline-block">ATC</div>
         </Link>
       </div>
-      <div className="w-2/4 flex justify-end items-center gap-5">
+      <div className="w-1/3 flex items-center">
+        <Link href="/tools">Tools</Link>
+        <Link href="/manufacturers">Manufacturers</Link>
+      </div>
+      <div className="w-1/3 flex justify-end items-center gap-5">
         {Boolean(user) ? null : 
           <Link href="/register">
             <Button gradientDuoTone="cyanToBlue">
