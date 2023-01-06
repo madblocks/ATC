24b2ee77ec2e0cec0ac5866f7b282d9adaa8b770
Andrew Kennedy <akennedy811@icloud.com>
2023-01-06 10:05:18 -0500

    removed layout from id pages

--- next-atc/pages/manufacturers/id.jsx
+++ next-atc/pages/manufacturers/id.jsx
@@ -1,11 +1,10 @@
-import DetailLayout from '../../components/detail_layout';
 import Head from 'next/head';
 
 export default function ManufacturerDetail () {
   return (
-    <DetailLayout>
+    <>
       <Head></Head>
       <div className="text-white">Manufacturer Details</div>
-    </DetailLayout>
+    </>
   )
 }
--- next-atc/pages/tools/id.jsx
+++ next-atc/pages/tools/id.jsx
@@ -1,11 +1,10 @@
-import DetailLayout from '../../components/detail_layout';
 import Head from 'next/head';
 
 export default function ToolDetail () {
   return (
-    <DetailLayout>
+    <>
       <Head></Head>
       <div className="text-white">Tool Details</div>
-    </DetailLayout>
+    </>
   )
 }
